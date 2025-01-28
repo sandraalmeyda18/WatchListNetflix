023d81c02c22c96e617f72771470b9237b6cc434
Sandra Almeyda <sandra.almeyda@aoshield.com>
2025-01-28 17:35:34 +0100

    add audiovisual controller

---
+++ WatchListNetflix.Server/Controllers/AudiovisualsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WatchListNetflix.Services.Audiovisuals;
+
+namespace WatchListNetflix.Server.Controllers
+{
+    [Route("api/audiovisuals")]
+    [ApiController]
+    public class AudiovisualsController : ControllerBase
+    {
+        private readonly IAudiovisualService _audiovisualService;
+
+        public AudiovisualsController(IAudiovisualService audiovisualService)
+        {
+            _audiovisualService = audiovisualService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAudiovisuals()
+        {
+            var audiovisual = await _audiovisualService.GetAllAsync();
+            return Ok(audiovisual);
+        }
+    }
+}
